3cd103e1516bc56ba9fcf69463c3798717e52a60
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2024-04-30 15:05:27 +0530

    Organize

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO.Compression;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     // Varibles
-    public float moveSpeed;
-    private float spriteRotation;
+    private float moveSpeed = 5f;
+
+    // Components Reference
     private SpriteRenderer Pacman;
+    private Rigidbody2D rb;
 
     // Gets Components Related To The Object
     public void Awake(){
         Pacman = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
@@ -26,30 +32,46 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
+        HandleInput();
+     
+    }
 
-        // KeyBindings
-        if (Input.GetKey(KeyCode.D)){
+
+
+    private void HandleInput(){
+
+	// RIGHT MOVEMENT
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
          
             transform.position += Vector3.right * moveSpeed * Time.deltaTime;
             Pacman.transform.rotation = Quaternion.Euler(0 , 0, 0);
 
         } 
-        else if (Input.GetKey(KeyCode.A)){
+	// LEFT MOVEMENT
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
 
             transform.position += Vector3.left * moveSpeed * Time.deltaTime;
             Pacman.transform.rotation = Quaternion.Euler(0 , 180, 0);
         } 
-        else if (Input.GetKey(KeyCode.W)){
+	// UP MOVEMENT
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)){
 
             transform.position += Vector3.up * moveSpeed * Time.deltaTime;
             Pacman.transform.rotation = Quaternion.Euler(0, 0 , 90);
 
         } 
-        else if (Input.GetKey(KeyCode.S)){
+	// DOWN MOVEMENT
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)){
 
             transform.position += Vector3.down * moveSpeed * Time.deltaTime;
             Pacman.transform.rotation = Quaternion.Euler(0, 0 , -90);
         }
+        else{
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+            Pacman.transform.rotation = Quaternion.Euler(0 , 0, 0);
+        }
+
+
     }
 
 }
