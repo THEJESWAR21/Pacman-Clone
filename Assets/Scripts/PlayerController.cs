b61f4e2a63fed08cd2e3c4302f22fafe96138072
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2024-04-30 13:25:24 +0530

    Sprite-Rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,31 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.Compression;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    // Varibles
     public float moveSpeed;
+    private float spriteRotation;
     private SpriteRenderer Pacman;
 
-
+    // Gets Components Related To The Object
     public void Awake(){
         Pacman = GetComponent<SpriteRenderer>();
     }
+
+
     // Start is called before the first frame update
-    void Start(){}
+    void Start(){
+
+    }
  
 
     // Update is called once per frame
     void Update(){
+
+        // KeyBindings
         if (Input.GetKey(KeyCode.D)){
          
             transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-            Pacman.flipX = false;
+            Pacman.transform.rotation = Quaternion.Euler(0 , 0, 0);
 
-        } else if (Input.GetKey(KeyCode.A)){
+        } 
+        else if (Input.GetKey(KeyCode.A)){
+
             transform.position += Vector3.left * moveSpeed * Time.deltaTime;
-            Pacman.flipX = true;
+            Pacman.transform.rotation = Quaternion.Euler(0 , 180, 0);
+        } 
+        else if (Input.GetKey(KeyCode.W)){
+
+            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+            Pacman.transform.rotation = Quaternion.Euler(0, 0 , 90);
+
+        } 
+        else if (Input.GetKey(KeyCode.S)){
+
+            transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+            Pacman.transform.rotation = Quaternion.Euler(0, 0 , -90);
         }
     }
 
