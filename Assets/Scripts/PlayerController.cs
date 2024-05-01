0f85bb08be0f4145bb449fd25eefcbf3b40c08aa
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2024-05-01 15:13:18 +0530

    WASD Controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,32 +14,47 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
-    {
-        // Input handling
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            direction = Vector2.up;
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            direction = -Vector2.up;
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            direction = -Vector2.right;
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            direction = Vector2.right;
-        }
+    private void Update(){
+
+     HandleInput();
     }
 
+   
     private void FixedUpdate()
     {
         // Movement
         rb.velocity = direction * speed;
     }     
+    
+    // Direction And Spirte Orientation Control
+     private void HandleInput(){
+
+
+           // Input handling
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) )
+        {
+            direction = Vector2.up;
+            transform.rotation = Quaternion.Euler(0,0,90);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            direction = -Vector2.up;
+            transform.rotation = Quaternion.Euler(0,0,-90);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            direction = -Vector2.right;
+            transform.rotation = Quaternion.Euler(0,180,0);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            direction = Vector2.right;
+            transform.rotation = Quaternion.Euler(0,0,0);
+        }
+
+
+    }
+
 
    }
 
