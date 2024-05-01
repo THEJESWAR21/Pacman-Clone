a91f4a7fdae8d37fa10486e702009d01510938a0
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2024-05-01 15:05:10 +0530

    Movement-BugFix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,77 +1,45 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.IO.Compression;
-using System.Runtime.CompilerServices;
-using JetBrains.Annotations;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.UIElements;
+
 
 public class PlayerController : MonoBehaviour
 {
-    // Varibles
-    private float moveSpeed = 5f;
+    public float speed = 5f; // Speed of Pacman
 
-    // Components Reference
-    private SpriteRenderer Pacman;
     private Rigidbody2D rb;
+    private Vector2 direction = Vector2.zero;
 
-    // Gets Components Related To The Object
-    public void Awake(){
-        Pacman = GetComponent<SpriteRenderer>();
+    private void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
     }
 
-
-    // Start is called before the first frame update
-    void Start(){
-
-    }
- 
-
-    // Update is called once per frame
-    void Update(){
-        HandleInput();
-     
-    }
-
-
-
-    private void HandleInput(){
-
-	// RIGHT MOVEMENT
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
-         
-            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-            Pacman.transform.rotation = Quaternion.Euler(0 , 0, 0);
-
-        } 
-	// LEFT MOVEMENT
-        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
-
-            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
-            Pacman.transform.rotation = Quaternion.Euler(0 , 180, 0);
-        } 
-	// UP MOVEMENT
-        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)){
-
-            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
-            Pacman.transform.rotation = Quaternion.Euler(0, 0 , 90);
-
-        } 
-	// DOWN MOVEMENT
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)){
-
-            transform.position += Vector3.down * moveSpeed * Time.deltaTime;
-            Pacman.transform.rotation = Quaternion.Euler(0, 0 , -90);
+    private void Update()
+    {
+        // Input handling
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            direction = Vector2.up;
         }
-        else{
-            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-            Pacman.transform.rotation = Quaternion.Euler(0 , 0, 0);
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direction = -Vector2.up;
         }
-
-
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direction = -Vector2.right;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            direction = Vector2.right;
+        }
     }
 
-}
+    private void FixedUpdate()
+    {
+        // Movement
+        rb.velocity = direction * speed;
+    }     
+
+   }
+
